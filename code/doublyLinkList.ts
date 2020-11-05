494f456fcdbdded591157d5a9f87d71a404efdcf
SKYsuiyuan123 <13011316073@163.com>
2020-11-05 22:05:16 +0800

    add: 双向链表补充

--- code/doublyLinkList.ts
+++ code/doublyLinkList.ts
@@ -2,13 +2,14 @@
  * @Author: sunpeiyuan
  * @Date: 2020-11-01 22:23:06
  * @LastEditors: sunpeiyuan
- * @LastEditTime: 2020-11-05 21:51:14
+ * @LastEditTime: 2020-11-05 22:04:40
  * @FilePath: \JS-DataStructureAndAlgorithm\code\doublyLinkList.ts
  * @Description: 链表 -- 双向链表
  */
 import { LinkedList } from "./linkedList";
 
 /**
+ *
  * 双向链表的特点：
  *  - 可以使用一个 head 和一个 tail 分别指向链表头部和尾部的节点。
  *  - 每个节点都由三部分组成：前一个节点的指针(prev)、保存的元素(element)、后一个节点的指针(next)。
@@ -46,7 +47,9 @@ export class DoublyLinkedList<T> extends LinkedList<T> {
 
   constructor() {
     // append, insert, removeAt 等方法，需要重写。
-    // get, indexOf, update, remove, isEmpty, size, getHead 等方法，可以复用父类的方法。
+    // indexOf, update, remove, isEmpty, size, getHead 等方法，可以复用父类的方法。
+
+    // get 方法，可以重写，也可以不重写。这里为了掩饰双向链表的特性，故重写。
     super();
   }
 
@@ -106,6 +109,9 @@ export class DoublyLinkedList<T> extends LinkedList<T> {
     }
 
     /** 普通情况 */
+    // 遍历时可以分为两种情况：
+    // 一种是 position < this.length / 2，此时 从前往后遍历。
+    // 一种是 position > this.length / 2，此时 从后往前遍历。
     if (position !== 0 && position !== this.length) {
       let index = 0;
       let current = this.head;
@@ -166,6 +172,9 @@ export class DoublyLinkedList<T> extends LinkedList<T> {
     }
 
     /** 普通情况 */
+    // 遍历时可以分为两种情况：
+    // 一种是 position < this.length / 2，此时 从前往后遍历。
+    // 一种是 position > this.length / 2，此时 从后往前遍历。
     if (position !== 0 && position !== this.length - 1) {
       let index = 0;
       let current = this.head;
@@ -184,5 +193,36 @@ export class DoublyLinkedList<T> extends LinkedList<T> {
 
     this.length--;
     return removeElement.element;
+  }
+
+  // get 遍历时 可以分为两种情况：
+  // 一种是 position < this.length / 2，此时 从前往后遍历快（跟父类方法一样）。
+  // 一种是 position > this.length / 2，此时 从后往前遍历快。
+  get(position: number) {
+    if (position < 0 || position > this.length - 1) return null;
+
+    // 将要查找到的节点
+    let current: DoublyNode<T> | null = null;
+
+    if (position < this.length / 2) {
+      let index = 0;
+      current = this.head;
+
+      while (index < position) {
+        current = current.next;
+        index++;
+      }
+    } else {
+      let index = this.length - 1;
+      current = this.tail;
+
+      while (index > position) {
+        current = current.prev;
+        index--;
+      }
+    }
+
+    // 此时 index === position
+    return current.element;
   }
 }
